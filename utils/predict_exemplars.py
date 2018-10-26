494058cff68d51e9043e6beaea4fbf194e9b382c
dylan <dylan.ebert@gmail.com>
2018-10-26 16:51:43 -0400

    .

--- utils/predict_exemplars.py
+++ utils/predict_exemplars.py
@@ -29,10 +29,41 @@ def fine_tuning(model):
         layer.trainable = True
     model.compile(optimizer=Adam(lr=.0001), loss='binary_crossentropy', metrics=['categorical_accuracy'])
 
-def predict():
+def predict_original():
+    predict_gen = datagen.flow_from_directory('/data/nlp/vae/model/gmc/images/original', target_size=(299, 299), batch_size=100, shuffle=False)
+    filenames = predict_gen.filenames
+    n = len(predict_gen)
+    for i in range(n):
+        print('{0} of {1}'.format(i+1, n), end='\r')
+        images, _ = predict_gen[i]
+        predictions = model.predict(images, verbose=0)
+        for j in range(len(images)):
+            filename = filenames[100 * i + j]
+            correct_label = os.path.basename(os.path.normpath(filename)).replace('.jpg','')
+            top_prediction_indices = np.argsort(predictions[j])[::-1]
+            with open(os.path.join('model/gmc/exemplars/original', filename.replace('.jpg','')), 'w+') as f:
+                for idx in top_prediction_indices:
+                    f.write('{0}\t{1}\n'.format(index_class_dict[idx], predictions[j][idx]))
+
+def predict_reconstruction():
+    predict_gen = datagen.flow_from_directory('/data/nlp/vae/model/gmc/images/reconstruction', target_size=(299, 299), batch_size=100, shuffle=False)
+    filenames = predict_gen.filenames
+    n = len(predict_gen)
+    for i in range(n):
+        print('{0} of {1}'.format(i+1, n), end='\r')
+        images, _ = predict_gen[i]
+        predictions = model.predict(images, verbose=0)
+        for j in range(len(images)):
+            filename = filenames[100 * i + j]
+            correct_label = os.path.basename(os.path.normpath(filename)).replace('.jpg','')
+            top_prediction_indices = np.argsort(predictions[j])[::-1]
+            with open(os.path.join('model/gmc/exemplars/reconstruction', filename.replace('.jpg','')), 'w+') as f:
+                for idx in top_prediction_indices:
+                    f.write('{0}\t{1}\n'.format(index_class_dict[idx], predictions[j][idx]))
+
+if __name__ == '__main__':
     datagen = ImageDataGenerator(rescale=1/255.)
     train_gen = datagen.flow_from_directory('/data/nlp/gmc/train', target_size=(299, 299), batch_size=100, shuffle=False)
-    predict_gen = datagen.flow_from_directory('/data/nlp/vae/model/gmc/images/original', target_size=(299, 299), batch_size=100, shuffle=False)
 
     n_classes = len(train_gen.class_indices)
 
@@ -42,29 +73,6 @@ def predict():
     model.load_weights('model/gmc/best.ckpt')
 
     index_class_dict = {k: v for v, k in train_gen.class_indices.items()}
-    filenames = predict_gen.filenames
-    n = len(predict_gen)
-    for i in range(n):
-        images, labels = predict_gen[i]
-        predictions = model.predict(images, verbose=1)
-        for j in range(len(images)):
-            top_prediction_labels = [index_class_dict[item] for item in np.argsort(predictions[j])[::-1]][:100]
-            print(filenames[100 * i + j])
-            print(top_prediction_labels)
 
-    '''index_class_dict = {k: v for v, k in train_gen.class_indices.items()}
-    filenames = gen.filenames
-    n = len(gen)
-    for i in range(n):
-        print('{0} of {1}'.format(i+1, n), end='\r')
-        images, labels = gen[i]
-        predictions = model.predict(images, verbose=0)
-        for j in range(len(images)):
-            correct_label = index_class_dict[np.argmax(labels[j])]
-            top_prediction_labels = [index_class_dict[item] for item in np.argsort(predictions[j])[::-1]][:100]
-            line = json.dumps({'label': str(correct_label), 'filename': filenames[batch_size * i + j], 'predictions': top_prediction_labels})
-            with open(os.path.join(predictions_path, correct_label + '.json'), 'a+') as f:
-                f.write('{0}\n'.format(line))'''
-
-if __name__ == '__main__':
-    predict()
+    predict_original()
+    predict_reconstruction()
